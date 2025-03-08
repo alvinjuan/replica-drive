299011e236d2ef84d3c90d487c7bd4f793ddb2b3
Alvin <alvinjasperj@gmail.com>
2025-03-08 12:20:32 -0800

    styled folders

--- src/components/google-drive/Dashboard.js
+++ src/components/google-drive/Dashboard.js
@@ -14,12 +14,19 @@ export default function Dashboard() {
       <Navbar />
       <Container fluid>
         <AddFolderButton currentFolder={folder} />
-        {childFolders.map(childFolder => (
-          <div key={childFolder.id} style={{ maxWidth: '250px' }}
-          className='p-2'>
-            <Folder folder={folder} />
+        {childFolders.length > 0 && (
+          <div className="d-flex flex-wrap">
+            {childFolders.map(childFolder => (
+              <div
+                key={childFolder.id}
+                style={{ maxWidth: "250px" }}
+                className="p-2"
+              >
+                <Folder folder={childFolder} />
+              </div>
+            ))}
           </div>
-        ))}
+        )}
       </Container>
     </>
   )
--- src/components/google-drive/Folder.js
+++ src/components/google-drive/Folder.js
@@ -1,6 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { Button } from 'react-bootstrap'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFolder } from '@fortawesome/free-solid-svg-icons'
 
-
-export default function Folder() {
-  return <div>{Folder.name}</div>
+export default function Folder({ folder }) {
+  return (
+    <Button 
+      variant='outline-dark' 
+      className='text-truncate w-100' 
+      as={Link}>
+    <FontAwesomeIcon icon={faFolder} className='me-2' />{folder.name}</Button>
+  )
 }
